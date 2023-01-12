b7ff15ccbaf4dd17467045dac6094b787415ecfa
YuryKorotin <yurykorotin73@gmail.com>
2023-01-12 13:34:52 +0800

    Complete colors xml generation

--- src/main/kotlin/domain_layer/usecases/android/GenerateComposeColorsUseCase.kt
+++ src/main/kotlin/domain_layer/usecases/android/GenerateComposeColorsUseCase.kt
@@ -2,20 +2,19 @@ package domain_layer.usecases.android
 
 import domain_layer.models.Color
 import domain_layer.usecases.MobileUtilUseCase
-import org.w3c.dom.Element
 import java.io.File
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.transform.OutputKeys
 import javax.xml.transform.TransformerFactory
 import javax.xml.transform.dom.DOMSource
 import javax.xml.transform.stream.StreamResult
+import kotlin.math.roundToInt
 
 class GenerateComposeColorsUseCase: MobileUtilUseCase<GenerateComposeColorsUseCase.Params, Unit> {
 
     companion object {
         const val COMPOSE_STYLE_CLASS_PACKAGE_NAME = "androidx.compose.ui.text"
         const val ANDROIDX_COMPOSE_ANNOTATION_PACKAGE_NAME = "androidx.compose.runtime"
-
     }
 
     data class Params(val packageName: String = "com.example.hello",
@@ -35,10 +34,18 @@ class GenerateComposeColorsUseCase: MobileUtilUseCase<GenerateComposeColorsUseCa
         document.appendChild(rootElement)
 
         colors.forEach { color ->
-            val em = document.createElement("color")
-            em.setAttribute("name", color.name)
-            em.appendChild(document.createTextNode("#000000"))
-            rootElement.appendChild(em)
+            val element = document.createElement("color")
+            element.setAttribute("name", color.name)
+            val redHex = "%02x".format((color.red * 255).roundToInt())
+            val greenHex = "%02x".format((color.green * 255).roundToInt())
+            val blueHex = "%02x".format((color.blue * 255).roundToInt())
+            val alphaHex = "%02x".format((color.alpha * 255).roundToInt())
+            if (color.alpha > 0) {
+                element.appendChild(document.createTextNode("#${alphaHex}${redHex}${greenHex}${blueHex}".uppercase()))
+            } else {
+                element.appendChild(document.createTextNode("#${redHex}${greenHex}${blueHex}".uppercase()))
+            }
+            rootElement.appendChild(element)
         }
 
         val transformerFactory = TransformerFactory.newInstance()
